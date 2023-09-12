e83ccdea8ff225f60ca8cef65c6fdf30bf1e1002
grayi <grayi@csc4252022.ssdt.berea.edu>
2023-09-12 00:59:34 -0400

    Finished for now

--- questnum2.c
+++ questnum2.c
@@ -7,17 +7,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
+#include <string.h>
 // when piping the parent is writing into the pipe whereas the child is reading
-//QUESTION 2: 
+//QUESTION 2:The parent is the only one whom writes. It appear that  in fact that the child does not open or write
+//It appears to output only is a blank character from the child.
+//In the fork parent is the only one with the reference to the open file, in forking, the child does not call its own open
+//command 
 
 int main(int argc, char *argv[]){
-//  I want this saved in a heap..malloc the array?
-//num 1 is x, parent set its to user input later
-//add scanf here later
 printf("hello world (pid:%d)\n", (int) getpid());
+int file=open("textfile.txt",O_CREAT,O_RDWR,S_IRWXO,O_TRUNC);
 int rc=fork(); //with  child created, consider using pipe() to the child..maybe use the wait call?
-int file=open("textfile.text",O_CREAT,O_RDWR,S_IRWXO); //Thesse were maybe brkoen cause of lack of file header..?
 if(rc<0){
 //fork failure, point and laugh!
 fprintf(stderr,"fork failed, laugh and clap now.\n");
@@ -26,15 +26,19 @@ exit(1);
 else if(rc==0){
 //child created, access value here NOW
 printf("We are currently in child");
-write(file,"I am from the child",1000000);
+char buffer[1024]="This text is from child";
+write(file,&buffer,strlen(buffer));
 //Our x, num1, is changed by child..
 }
 
 else{ //parent goes here, should run only after child changes num1
 int rc_wait=wait(NULL);
 printf("We are returned to parent");
-write(file,"I am from the parent",1000000);
+char buffer[1024]="This text is from the parent";
+write(file,&buffer,strlen(buffer));
+
 printf("hello, I am parent of %d (rc_wait:%d) (pid:%d)\n",rc, rc_wait, (int) getpid());
+close(file);
 }
 return 0;
 }
